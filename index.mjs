c67e1e85014486d92c50dbd7d4ac8ca43357c233
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-04-29 16:45:07 +0200

    add lucky-draw

--- index.mjs
+++ index.mjs
@@ -1,12 +1,29 @@
-import * as fs from "fs";
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
 
-const fileName = "example.txt";
-const fileContents = "Lorem Ipsum";
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
 
-fs.writeFile(fileName, fileContents, function (err) {
-  if (err) {
-    console.error(`Failed to write file: ${err}`);
-    return;
-  }
-  console.log(`File ${fileName} written successfully!`);
-});
+luckyDraw('Joe')
+  .then((result) => {
+    console.log(result);
+    return luckyDraw('Caroline');
+  })
+  .then((result) => {
+    console.log(result);
+    return luckyDraw('Sabrina');
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
